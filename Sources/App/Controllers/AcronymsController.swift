a8c4667844e99171de3df8fea70175ea904ba5d6
Kaique Magno dos Santos <kaique.magno@hotmail.com>
2018-08-23 13:37:44 -0300

    Chapter 9 - Parent Child Relationships | Querying the relationships

--- Sources/App/Controllers/AcronymsController.swift
+++ Sources/App/Controllers/AcronymsController.swift
@@ -7,6 +7,7 @@ struct AcronymsController: RouteCollection {
     func boot(router: Router) throws {
         let acronymsRouter = router.grouped("api","acronyms")
         
+        //Register router HTTP verb's to Handlers
         acronymsRouter.post(Acronym.self, use: createHandler)
         acronymsRouter.get(use: getAllHandler)
         acronymsRouter.get(Acronym.parameter, use: getAllHandler)
@@ -15,6 +16,8 @@ struct AcronymsController: RouteCollection {
         acronymsRouter.get("search", use: searchHandler)
         acronymsRouter.get("first", use: getFirstHandler)
         acronymsRouter.get("sorted", use: sortedHandler)
+        
+        acronymsRouter.get(Acronym.parameter,"user", use: getUserHandler)
     }
     
     // MARK: - Handlers
@@ -34,6 +37,7 @@ struct AcronymsController: RouteCollection {
         return try flatMap(to: Acronym.self, request.parameters.next(Acronym.self), request.content.decode(Acronym.self), { (acronym, updatedAcronym) -> EventLoopFuture<Acronym> in
             acronym.short = updatedAcronym.short
             acronym.long = updatedAcronym.long
+            acronym.userID = updatedAcronym.userID
             return acronym.save(on: request)
         })
     }
@@ -67,5 +71,12 @@ struct AcronymsController: RouteCollection {
     
     func sortedHandler(_ request:Request) throws -> Future<[Acronym]> {
         return Acronym.query(on: request).sort(\.short, .ascending).all()
+    }
+    
+    // MARK: Relations Handler's
+    func getUserHandler(_ request: Request) throws -> Future<User> {
+        return try request.parameters.next(Acronym.self).flatMap(to: User.self, { (acronym) -> EventLoopFuture<User> in
+            return acronym.user.get(on: request)
+        })
     }
 }
--- Sources/App/Models/Acronym.swift
+++ Sources/App/Models/Acronym.swift
@@ -5,10 +5,20 @@ final class Acronym:Codable {
     var id:Int?
     var short:String?
     var long:String?
+    var userID: User.ID
     
-    init(short:String,long:String) {
+    init(short:String, long:String, userID:User.ID) {
         self.short = short
         self.long = long
+        self.userID = userID
+    }
+}
+
+extension Acronym {
+    //Add a computed property to Acronym to get the User object of the acronym’s owner. This returns Fluent’s generic Parent type
+    var user: Parent<Acronym,User> {
+        //Use Fluent’s parent(_:) function to retrieve the parent. This takes the key path of the user reference on the acronym
+        return parent(\.userID)
     }
 }
 
